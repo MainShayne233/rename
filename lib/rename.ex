d7d289ead4060cdf88139d32df4584c38c1dd313
MainShayne233 <shaynetremblay@hotmail.com>
2017-04-15 01:44:42 -0400

    adds docs

--- lib/rename.ex
+++ lib/rename.ex
@@ -1,5 +1,15 @@
 defmodule Rename do
 
+  @moduledoc """
+  The single module that does all the renaming
+  Talk about pressure
+  At least there are tests
+  Well there's "a" test
+  But it does like one thing
+  So it's fine
+  If you're mad about it, submit a PR.
+  """
+
   @default_extensions [
     "ex",
     "exs",
@@ -16,6 +26,11 @@ defmodule Rename do
   @default_starting_directory "."
 
   @default_ignore_files []
+
+  @doc """
+  The public function you use to rename your app.
+  Call looks like: run({"OldName", "NewName"}, {"old_otp", "new_otp"}, options)
+  """
 
   def run(names, opts, options \\ [])
   def run(names = {_old_name, _new_name}, otps = {_old_otp, _new_otp}, options) do
@@ -35,8 +50,7 @@ defmodule Rename do
     """
   end
 
-
-  def rename_in_directory(names = {old_name, new_name}, otps = {old_otp, new_otp}, cwd, options) do
+  defp rename_in_directory(names = {old_name, new_name}, otps = {old_otp, new_otp}, cwd, options) do
     cwd
     |> File.ls!
     |> Enum.each(fn path ->
@@ -60,18 +74,18 @@ defmodule Rename do
     end)
   end
 
-  def is_valid_directory?(dir, options) do
+  defp is_valid_directory?(dir, options) do
     File.dir?(dir) and
     dir in (options[:ignore_directories] || @default_ignore_directories) == false
   end
 
-  def is_valid_file?(file, options) do
+  defp is_valid_file?(file, options) do
     File.exists?(file) and
     file in (options[:ignore_files] || @default_ignore_files) == false and
     has_valid_extension?(file, options)
   end
 
-  def has_valid_extension?(file, options) do
+  defp has_valid_extension?(file, options) do
     extension = file
     |> String.split(".")
     |> List.last
