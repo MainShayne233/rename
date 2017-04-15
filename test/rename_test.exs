6554ff8d86d1dca486580922e263c209f364bd5c
MainShayne233 <shaynetremblay@hotmail.com>
2017-04-15 01:21:17 -0400

    test copies files

--- test/rename_test.exs
+++ test/rename_test.exs
@@ -1,29 +1,42 @@
 defmodule RenameTest do
   use ExUnit.Case
 
+  @test_copy_dir "test_copy"
+  @old_app_name "Rename"
+  @old_app_otp "rename"
+  @new_app_name "ToDoTwitterClone"
+  @new_app_otp "to_do_twitter_clone"
+  
+
   test "should properly rename app with default options" do
     create_copy_of_app()
     Rename.run(
-      {"Rename", "ToDoTwitterClone"}, 
-      {"rename", "to_do_twitter_clone"}, 
-      starting_directory: "./test_copy"
+      {@old_app_name, @new_app_name}, 
+      {@old_app_otp, @new_app_otp}, 
+      starting_directory: @test_copy_dir
     )
-    delete_copy_of_app()
+    #    assert File.read!(@test_copy_dir <> "/mix.exs") |> String.contains?(@new_app_name)
+    #    assert File.read!(@test_copy_dir <> "/mix.exs") |> String.contains?(@old_app_name) == false
+    #    assert File.dir?(@test_copy_dir <> "./lib/" <> @new_app_otp <> ".ex")
+    #    delete_copy_of_app()
   end
 
 
   defp create_copy_of_app do
-    File.mkdir("test_copy")
+    File.mkdir(@test_copy_dir)
     File.ls!
     |> Enum.each(fn path -> 
-      if File.dir?(path) and not_ignored_path(path) do
-        System.cmd("cp", ["-r", path, "test_copy"])
+      cond do
+        File.dir?(path) and not_ignored_path(path) ->
+          System.cmd("cp", ["-r", path, @test_copy_dir])
+        true ->
+          File.cp!(path, @test_copy_dir)
       end
     end)
   end
 
   defp delete_copy_of_app() do
-    System.cmd("rm", ["-rf", "test_copy"])
+    System.cmd("rm", ["-rf", @test_copy_dir])
   end
 
 
@@ -31,7 +44,7 @@ defmodule RenameTest do
     [
       "_build",
       "deps", 
-      "test_copy",
+      @test_copy_dir,
       ".git",
     ]
     |> Enum.find(&(&1 == path)) == nil
